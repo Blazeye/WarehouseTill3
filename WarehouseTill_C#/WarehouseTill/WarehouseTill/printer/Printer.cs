12b0a76912e727192b2838836a63c2e070448ee8
Blazeye <StJacobstr14@gmail.com>
2019-09-03 12:58:10 +0200

    added printer and warehouse which get activated through events

---
+++ WarehouseTill_C#/WarehouseTill/WarehouseTill/printer/Printer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using WarehouseTill.model;
+
+namespace WarehouseTill.printer
+{
+    class Printer
+    {
+
+        public Dictionary<string, OrdersProduct> CheckoutDict = new Dictionary<string, OrdersProduct>();
+
+        public void PrintItemList(Dictionary<string, OrdersProduct> dict)
+        {
+            Console.WriteLine("================ PRINTING: ===============");
+            foreach(string item in dict.Keys)
+            {
+                switch (item)
+                {
+                    case "1234":
+                        PrintButter(dict[item].Amount);
+                        break;
+                    case "9902":
+                        PrintCheese(dict[item].Amount);
+                        break;
+                    case "3568":
+                        PrintMilk(dict[item].Amount);
+                        break;
+                    case "7324":
+                        PrintEggs(dict[item].Amount);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("==========================================\n");
+        }
+
+        public void PrintButter(int amount)
+        {
+            Console.Out.WriteLine("|                                         |");
+            Console.Out.WriteLine($"|    _________________________  AMOUNT: {amount} |");
+            Console.Out.WriteLine("|   /                        /|           |");
+            Console.Out.WriteLine("|  /     /3onko-/3oter      / |           |");
+            Console.Out.WriteLine("| /_______________________ /  :           |");
+            Console.Out.WriteLine("| |                        |  /           |");
+            Console.Out.WriteLine("| |                        | /            |");
+            Console.Out.WriteLine("| |________________________|/             |");
+            Console.Out.WriteLine("|                                         |");
+        }
+
+        public void PrintEggs(int amount)
+        {
+        Console.Out.WriteLine("|                .-~-.                    |");
+        Console.Out.WriteLine($"|              .\'     \'.        AMOUNT: {amount} |");
+        Console.Out.WriteLine("|             /         \\                 |");
+        Console.Out.WriteLine("|     .-~-.  :           ;                |");
+        Console.Out.WriteLine("|   .\'     \'.|           |                |");
+        Console.Out.WriteLine("|  /         \\           :                |");
+        Console.Out.WriteLine("| :           ; .-~\"\"~-,/                 |"); 
+        Console.Out.WriteLine("| |           /`        `\'.               |");
+        Console.Out.WriteLine("| :          |             \\              |"); 
+        Console.Out.WriteLine("|  \\         |             /              |");
+        Console.Out.WriteLine("|   `.     .\' \\          .\'               |");
+        Console.Out.WriteLine("|     `~~~`    \'-.____.-\'                 |");
+        Console.Out.WriteLine("|                                         |");
+        }
+        public void PrintCheese(int amount)
+        {
+        Console.Out.WriteLine("|     _--\"-.                              |");
+        Console.Out.WriteLine($"|  .-\"      \"-.                 AMOUNT: {amount} |");
+        Console.Out.WriteLine("| | \"\"--..      \'-.                       |");
+        Console.Out.WriteLine("| |       \"\"--..  \'-.                     |");
+        Console.Out.WriteLine("| |.-.  .-\".   \"\"--..\".                   |");
+        Console.Out.WriteLine("| |'./  -_ \' .-.      |                   |");
+        Console.Out.WriteLine("| |      .-. \'.-\'   .-\'                   |");
+        Console.Out.WriteLine("| '--..  \'.\'    .-  -.                    |");
+        Console.Out.WriteLine("|     \"\"--..    \'_\'   :                   |");
+        Console.Out.WriteLine("|          \"\"--..     |                   |");
+        Console.Out.WriteLine("|                \"\" - \'                   |");
+        Console.Out.WriteLine("|                                         |");
+
+        }
+        public void PrintMilk(int amount)
+        {
+        Console.Out.WriteLine("|     ________                            |");
+        Console.Out.WriteLine($"|    j________j                 AMOUNT: {amount} |");
+        Console.Out.WriteLine("|   /________/_\\                          |");
+        Console.Out.WriteLine("|  |         |  |                         |");
+        Console.Out.WriteLine("|  | |\\/|ELK |  |                         |");
+        Console.Out.WriteLine("|  |         |  |                         |");
+        Console.Out.WriteLine("|  |  _(~)_  |  |                         |");
+        Console.Out.WriteLine("|  |   )\"(   |  |                         |");
+        Console.Out.WriteLine("|  |  (@_@)  |  |                         |");
+        Console.Out.WriteLine("|  |         |  |                         |");
+        Console.Out.WriteLine("|  | _______ |,/\'                         |");
+        Console.Out.WriteLine("|                                         |");
+        }
+
+        public void HandlePrintItems(object s, OrdersListEventArgs e)
+        {
+            CheckoutDict = e.OrderList;
+            PrintItemList(CheckoutDict);
+        }
+    }
+}
---
+++ WarehouseTill_C#/WarehouseTill/WarehouseTill/warehouse/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseTill.warehouse
+{
+    public class Item
+    {
+        //public event EventHandler AddProduct;
+
+        //protected virtual void Raise_AddProduct(ItemEventArgs e)
+        //{
+        //    EventHandler handler = AddProduct;
+
+        //    if (handler != null)
+        //    {
+        //        handler(this, e);
+        //    }
+
+            //can also use:
+            //handler?.Invoke(this, e);
+
+            //if event is triggered, and you want to pass the reference with no additional data over to Warehouse:
+            //if (handler != null)
+            //{
+            //    handler(this, EventArgs.Empty);
+            //}
+        //}
+    }
+}
